c575743aa54343067b7140837a6260094c3cf73b
Zachary Richardson <zackrichardson97@gmail.com>
2019-04-18 01:36:01 -0700

    Added file validation in driver.

--- driver.py
+++ driver.py
@@ -1,6 +1,7 @@
 # Driver for ML component of MarkLogic Classifier.
 import sys
 import classifier as cfr
+import os.path
 
 # Params: ontology = stuff passed by frontend
 #         filepaths = list of filepaths to CSV files
@@ -11,6 +12,9 @@ def classify(ontology, filepaths):
     if len(filepaths) == 0 or len(ontology) != 2:
         print_usage()
         raise ValueError("Bad arguments for Driver.classify")
+    for filepath in filepaths:
+        if not os.path.isfile(filepath):
+           raise FileNotFoundError(str(filepath) + " couldn't be found.")
     my_classifier = cfr.Classifier(filepaths)
     results_json = my_classifier.classify_ontology(ontology)
     return results_json
